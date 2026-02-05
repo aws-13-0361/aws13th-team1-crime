ccad7c0c36894546155749ec3df8af991189c3fd
eewwqq1 <rlaehdrbs90@gmail.com>
2026-02-05 09:50:58 +0900

    pull 완료

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
-from app.api.v1.endpoints import report_router, official_stats_endpoint
+from router import report_router, official_stats_endpoint
 from utils.database import get_db
-from models import User, Region, CrimeType, Report
-import uvicorn
+from models import Region, CrimeType
 from router.admin_router import router as admin_router
 
 app = FastAPI()
@@ -42,6 +41,3 @@ def get_crime_types(db: Session = Depends(get_db)):
 def root():
     return {"message": "API Server is running"}
 
-if __name__ == "__main__":
-    # 서버 실행: uvicorn run:app --reload 와 동일한 효과
-    uvicorn.run("run:app", host="127.0.0.1", port=8000, reload=True)
